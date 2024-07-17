511961d4b8c7151294c621a66bc97cdc45934e76
Alvi aditia <forwadersms@gmail.com>
2024-07-17 10:01:32 +0000

    Edited with DroidScript

--- UI Droidscript v1.js
+++ UI Droidscript v1.js
@@ -1,16 +1,44 @@
-
-//Called when application is started.
-function OnStart()
+class Main extends App
 {
-	//Create a layout with objects vertically centered.
-	lay = app.CreateLayout( "Linear", "VCenter,FillXY" )
+    onStart()
+    {
+        // Creates a layout with objects verticaly centered.
+        this.main = ui.addLayout( "main", "Linear", "VCenter", 1, 1 )
+        this.main.setChildMargins( 0.01, 0.01, 0.01, 0.01 )
 
-	//Create a text label and add it to layout.
-	txt = app.CreateText( "Hello" )
-	txt.SetTextSize( 32 )
-	lay.AddChild( txt )
-	
-	//Add layout to app.	
-	app.AddLayout( lay )
+        var navs = [
+            [ "Favorites", "favorites" ],
+            [ "Groups", "person" ],
+            [ "Folder", "folder" ]
+        ]
+
+        // Creates a BottomNavbar control
+        this.bmn = ui.addBottomNavbar(this.main, navs, "", 1)
+
+        // Add a callback handler for `onChange` event
+        this.bmn.setOnChange( this.onChange )
+
+        // Button to show the labels
+        this.btn = ui.addButton(this.main, "Show Labels")
+        this.btn.setOnTouch( this.showLabels )
+
+        // Button to hide the labels
+        this.btn1 = ui.addButton(this.main, "Hide Labels")
+        this.btn1.setOnTouch( this.hideLabels )
+    }
+
+    showLabels( )
+    {
+        this.bmn.showLabels()
+    }
+
+    hideLabels()
+    {
+        this.bmn.hideLabels()
+    }
+
+    onChange(text, index)
+    {
+        ui.showPopup( text )
+    }
 }
-
