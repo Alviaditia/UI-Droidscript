e3c4c2019b9ed95890c506a77d5d273e95bb68b2
Alvi aditia <forwadersms@gmail.com>
2024-07-21 03:43:19 +0000

    Edited with DroidScript add 5lay

--- UI Droidscript v1.js
+++ UI Droidscript v1.js
@@ -5,30 +5,68 @@ function OnStart()
     // Buat layout utama.
     layMain = app.CreateLayout("Absolute", "FillXY");
     
-    // Buat dan tambahkan layout kedua.
-    lay2 = app.AddLayout(layMain,"Linear", "Vertical");
-    lay2.SetBackColor("#00ffff");
-    lay2.SetPosition(0, 0, 1, 1); // Posisi dan ukuran: (x, y, width, height)
+    // Buat dan tambahkan layout navigasi.
+    layNav = app.CreateLayout("Linear", "Horizontal,Top");
+    layNav.SetBackColor("red");
+    layNav.SetPosition(0, 0.93, 1, 0.07); // Posisi dan ukuran: (x, y, width, height)
+    layMain.AddChild(layNav);
     
-    // Buat dan tambahkan layout ketiga.
-    lay3 = app.AddLayout(layMain,"Linear", "Horizontal,Top");
-    lay3.SetBackColor("red");
-    lay3.SetPosition(0, 0.93, 1, 1); // Posisi dan ukuran: (x, y, width, height)
+    // Buat layout konten.
+    lay1 = app.CreateLayout("Linear", "Vertical");
+    lay1.SetBackColor("#00ffff");
+    lay1.SetPosition(0, 0, 1, 0.93);
+    layMain.AddChild(lay1);
     
-    // Tambahkan ikon FontAwesome ke layout ketiga.
+    lay2 = app.CreateLayout("Linear", "Vertical");
+    lay2.SetBackColor("#ff00ff");
+    lay2.SetPosition(0, 0, 1, 0.93);
+    layMain.AddChild(lay2);
+    
+    lay3 = app.CreateLayout("Linear", "Vertical");
+    lay3.SetBackColor("#ffff00");
+    lay3.SetPosition(0, 0, 1, 0.93);
+    layMain.AddChild(lay3);
+    
+    lay4 = app.CreateLayout("Linear", "Vertical");
+    lay4.SetBackColor("#00ff00");
+    lay4.SetPosition(0, 0, 1, 0.93);
+    layMain.AddChild(lay4);
+    
+    lay5 = app.CreateLayout("Linear", "Vertical");
+    lay5.SetBackColor("#0000ff");
+    lay5.SetPosition(0, 0, 1, 0.93);
+    layMain.AddChild(lay5);
+    
+    // Tambahkan teks ke setiap layout.
+    var text1 = app.CreateText("Ini layout 1");
+    lay1.AddChild(text1);
+    
+    var text2 = app.CreateText("Ini layout 2");
+    lay2.AddChild(text2);
+    
+    var text3 = app.CreateText("Ini layout 3");
+    lay3.AddChild(text3);
+    
+    var text4 = app.CreateText("Ini layout 4");
+    lay4.AddChild(text4);
+    
+    var text5 = app.CreateText("Ini layout 5");
+    lay5.AddChild(text5);
+    
+    // Tambahkan ikon FontAwesome ke layout navigasi.
     nav = [
-        app.AddText(lay3, " [fa-database] ", -1, -1, "fontAwesome"),
-        app.AddText(lay3, "[fa-dashboard]", -1, -1, "fontAwesome"),
-        app.AddText(lay3, "[fa-plus-square-o]", -1, -1, "fontAwesome"),
-        app.AddText(lay3, "[fa-pie-chart]", -1, -1, "fontAwesome"),
-        app.AddText(lay3, "[fa-user]", -1, -1, "fontAwesome"),
+        app.AddText(layNav, " [fa-database] ", -1, -1, "fontAwesome"),
+        app.AddText(layNav, "[fa-dashboard]", -1, -1, "fontAwesome"),
+        app.AddText(layNav, "[fa-plus-square-o]", -1, -1, "fontAwesome"),
+        app.AddText(layNav, "[fa-pie-chart]", -1, -1, "fontAwesome"),
+        app.AddText(layNav, "[fa-user]", -1, -1, "fontAwesome"),
     ];
     
     // Warna awal ikon.
     var defaultColor = "white";
     
-    // Fungsi untuk mengubah warna ikon.
-    function changeColor(clickedIcon) {
+    // Fungsi untuk mengubah warna ikon dan menampilkan layout yang sesuai.
+    function changeColor(clickedIcon, layoutToShow) {
         requestAnimationFrame(function() {
             for (var i = 0; i < nav.length; i++) {
                 if (nav[i] === clickedIcon) {
@@ -38,53 +76,44 @@ function OnStart()
                     nav[i].SetTextColor(defaultColor);
                 }
             }
+            lay1.Hide();
+            lay2.Hide();
+            lay3.Hide();
+            lay4.Hide();
+            lay5.Hide();
+            layoutToShow.Show();
         });
     }
     
-    for (var i = 0; i < nav.length; i++) {
+       for (var i = 0; i < nav.length; i++) {
         nav[i].SetTextColor(defaultColor);
         nav[i].SetTextSize(36);
         nav[i].SetMargins(0.050, 0.01, 0.050, 0.01);
         
-        // Tambahkan event listener untuk mengubah warna saat diklik.
-        nav[i].SetOnTouch(function() {
-            changeColor(this);
-        });
     }
-    
-    // Tambahkan event listener untuk memanggil fungsi Chart saat nav[1] diklik.
-        nav[1].SetOnTouch(function() {
-        changeColor(this);
-        Chart();
+    // Tambahkan event listener untuk setiap ikon navigasi.
+  nav[0].SetOnTouch(function() {
+        changeColor(this, lay1);
+    });
+    nav[1].SetOnTouch(function() {
+        changeColor(this, lay2);
+    });
+    nav[2].SetOnTouch(function() {
+        changeColor(this, lay3);
+    });
+    nav[3].SetOnTouch(function() {
+        changeColor(this, lay4);
+    });
+    nav[4].SetOnTouch(function() {
+        changeColor(this, lay5);
     });
     
-    // Tambahkan layout utama ke aplikasi.
+    // Tampilkan layout utama.
     app.AddLayout(layMain);
     
+    // Sembunyikan semua layout kecuali yang pertama.
+    lay2.Hide();
+    lay3.Hide();
+    lay4.Hide();
+    lay5.Hide();
 }
-
-
-
-
-app.LoadPlugin('ChartJS');
-function Chart()
-{
-    if (!chartExists) {//cek apakah chart sudah ada
-        chart = app.LoadChartJS();
-        
-        data = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [2, 5, 3, 15, 10],
-                    backgroundColor: ["#9C27B0", "#5E35B1", "#039BE5", "#FF9800", "#26A69A"]
-                }
-            ]
-        };
-        
-        pie = chart.CreateChart(data, 'pie', 0.9, 0.5);
-        lay2.AddChild(pie);
-        
-        chartExists = true;
-    }}
