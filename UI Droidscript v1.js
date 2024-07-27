1a2bd334ec6120afeb8965fc0916c02363aa15d7
Alvi aditia <forwadersms@gmail.com>
2024-07-27 13:58:09 +0000

    Edited with DroidScript2058

--- UI Droidscript v1.js
+++ UI Droidscript v1.js
@@ -63,7 +63,7 @@ function OnStart()
     ];
     
     // Warna awal ikon.
-    var defaultColor = "green";
+    var defaultColor = "black";
      for (var i = 0; i < nav.length; i++) {
         nav[i].SetTextColor(defaultColor);
         nav[i].SetTextSize(36);
